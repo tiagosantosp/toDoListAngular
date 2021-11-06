14a44ebbb158045b4765775c32cc102bf272eb49
Tiago Santos <tiago_s.p@hotmail.com>
2021-11-06 16:52:31 -0300

    todoListAngular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
+import { Todo } from './../models/todos.model';
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,84 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'todo';
+  public mode: string = 'list'
+  public todos: Todo[] = []
+  public title: string = 'Minhas tarefas'
+  public form: FormGroup 
+
+  constructor(private fb: FormBuilder) {
+    this.form = this.fb.group({
+      // primeiro parametro referente ao valor padrão do campo
+      // segundo parametro qual propriedade será adicionada ao form
+      title: ['',  Validators.compose([ // compose serve para adicionar varios validadores
+        Validators.minLength(3),// minimo de caracteres
+        Validators.maxLength(60),// maximo de caracteres
+        Validators.required // campo obrigatório
+      ])]
+    })    
+    
+    this.load()
+  }
+
+  // Adicionar tarefa
+  add() {
+    const title = this.form.controls['title'].value //pegar valor do title 
+    const id = this.todos.length + 1
+    this.todos.push(new Todo(title, false, id))
+    this.save()
+    this.clear()
+  }
+
+  //limpar formulário
+  clear() {
+    this.form.reset()
+  }
+
+
+  // Remover tarefa
+  remove(todo: Todo) {
+    const index = this.todos.indexOf(todo)
+    if (index !== -1) {
+      this.todos.splice(index, 1)
+    }
+
+    this.save()
+  }
+
+  // Marcar tarefa como concluida
+  markAsDone(todo: Todo) {
+    todo.done = true
+
+    this.save()
+  }
+
+  // Marcar tarefa como não concluida
+  markAsUndone(todo: Todo) {
+    todo.done = false
+
+    this.save()
+  }
+
+  // gravar dados no LocalStorage
+  save() {
+    const data = JSON.stringify(this.todos)
+    localStorage.setItem('todos', data)
+    this.mode = 'list'
+  }
+
+  // Carregar dados salvos no LocalStorage
+  load() {
+    const data = localStorage.getItem('todos')
+    if (data) {
+      this.todos = JSON.parse(data)
+    } else {
+      this.todos = []
+    }
+  }
+
+  // alterar tipo de modal
+  changeMode( modo: string ) {
+    this.mode = modo
+  }
+
 }
---
+++ src/models/todos.model.ts
@@ -0,0 +1,10 @@
+export class Todo {
+
+    constructor(
+        public title: String,
+        public done: Boolean,
+        public id: number) {
+
+
+    }
+}
